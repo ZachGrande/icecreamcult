de12d69ff06c962012cbb98f833a879a0966a4e3
Zach Grande <zach.grande23@gmail.com>
2025-01-27 07:52:28 -0800

    feat: add landing animation, biography, spotify embed, soundcloud link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,7 @@
-'use client'
-
-import { useEffect, useState } from "react";
-
 export default function Home() {
-  const [showElements, setShowElements] = useState(false);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowElements(true);
-    }, 500);
-    return () => clearTimeout(timer);
-  }, []);
 
   return (
-    <footer className={`row-start-3 flex gap-6 flex-wrap items-center justify-center ${showElements ? 'animate-fadeIn' : 'opacity-0'}`}>
+    <footer className={`row-start-3 flex gap-6 flex-wrap items-center justify-center animate-fadeIn`}>
       <a
         href="https://www.instagram.com/icecreamcult/"
         target="_blank"
@@ -27,6 +15,14 @@ export default function Home() {
         rel="noopener noreferrer"
       >
         <i className="fa-solid fa-envelope text-4xl"></i>
+      </a>
+
+      <a
+        href="https://soundcloud.com/icecreamcult"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="fa-brands fa-soundcloud text-4xl"></i>
       </a>
     </footer>
   );
