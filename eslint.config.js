b2d5ce654aa6e981aa336956444aa426ad0dfad0
Zach Grande <zach.grande23@gmail.com>
2026-05-16 16:37:17 -0700

    chore: revert patch from failed eslint upgrade

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,7 @@ import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 import nextTypescript from "eslint-config-next/typescript";
 
 const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, {
-  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"],
-  settings: {
-    react: { version: "19" }
-  }
+  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
 }];
 
 export default eslintConfig;
